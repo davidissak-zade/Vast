d84854ee0b901ff3b9269e08aa89f8af659a7ff0
David Issak-Zade <david.issak-zade@mail.ru>
2022-07-21 16:47:14 +0300

    merge conflict tackled
    merged Katia's code

--- app/src/main/java/com/example/eskillsandroid/MainActivity.java
+++ app/src/main/java/com/example/eskillsandroid/MainActivity.java
@@ -1,38 +1,3 @@
-// package com.example.vastandroid;
-//
-//import androidx.appcompat.app.AppCompatActivity;
-//import androidx.fragment.app.FragmentManager;
-//import androidx.fragment.app.FragmentTransaction;
-//
-//import android.os.Bundle;
-//import android.view.View;
-//import android.widget.Button;
-//
-//import com.example.eskillsandroid.fragments.DiscussionsFragment;
-//import com.example.eskillsandroid.fragments.HomeFragment;
-//import com.example.eskillsandroid.fragments.SearchFragment;
-//
-// public class MainActivity extends AppCompatActivity {
-//     private Button button;
-//
-//     @Override
-//     protected void onCreate(Bundle savedinstanceState){
-//         super.onCreate(savedinstanceState);
-//         setContentView(R.layout.activity_main);
-//
-//         button = (Button)findViewById(R.id.button);
-//         button.setOnClickListener(new View.OnClickListener)(){
-//             @Override
-//             public void onClick(View v){
-//                 openviewpage();
-//             });
-//     }
-
-
-
-
-//    }
-//}
 
 package com.example.eskillsandroid;
 
@@ -43,50 +8,30 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-import com.example.eskillsandroid.fragments.DiscussionsFragment;
-import com.example.eskillsandroid.fragments.HomeFragment;
-import com.example.eskillsandroid.fragments.SearchFragment;
-=======
->>>>>>> Stashed changes
 
-public class MainActivity extends AppCompatActivity {
-=======
- public class MainActivity extends AppCompatActivity {
->>>>>>> Stashed changes
-    private Button button;
+public  class MainActivity extends AppCompatActivity {
+    Button button, button2;
+
     @Override
     protected void onCreate(Bundle savedinstanceState) {
         super.onCreate(savedinstanceState);
         setContentView(R.layout.activity_main);
 
-<<<<<<< Updated upstream
-        button = findViewById(R.id.button1);
+        button = findViewById(R.id.CV);
+        button2 = findViewById(R.id.bank);
 
-=======
-        button = (Button) findViewById(R.id.button1);
-        button.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                openviewpage();
-            }
-        });
+
     }
 
-     private void openviewpage() {
-     }
 
-     public void  opennviewpage(){
-        Intent intent = new Intent(this, viewpage.class);
+    public void OnClick(View v ) {
+        Intent intent = new Intent(MainActivity.this, viewpage.class);
         startActivity(intent);
->>>>>>> Stashed changes
+
     }
-    public void Onclick(View view){
-        Intent intent = new Intent (this, viewpage.class);
-        startActivity(intent);
-    }
+
 }
+
 
 
 
