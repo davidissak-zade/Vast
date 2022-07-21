a4e1bd24b25f9d3e4e366237fbafdfe41f78c7ca
katiazacharia <109578632+katiazacharia@users.noreply.github.com>
2022-07-21 07:33:24 +0300

    Pushing remaining changes from Katia's computer
    Allowing devs to split off

--- app/src/main/java/com/example/eskillsandroid/MainActivity.java
+++ app/src/main/java/com/example/eskillsandroid/MainActivity.java
@@ -1,23 +1,33 @@
- package com.example.eskillsandroid;
+// package com.example.vastandroid;
+//
+//import androidx.appcompat.app.AppCompatActivity;
+//import androidx.fragment.app.FragmentManager;
+//import androidx.fragment.app.FragmentTransaction;
+//
+//import android.os.Bundle;
+//import android.view.View;
+//import android.widget.Button;
+//
+//import com.example.eskillsandroid.fragments.DiscussionsFragment;
+//import com.example.eskillsandroid.fragments.HomeFragment;
+//import com.example.eskillsandroid.fragments.SearchFragment;
+//
+// public class MainActivity extends AppCompatActivity {
+//     private Button button;
+//
+//     @Override
+//     protected void onCreate(Bundle savedinstanceState){
+//         super.onCreate(savedinstanceState);
+//         setContentView(R.layout.activity_main);
+//
+//         button = (Button)findViewById(R.id.button);
+//         button.setOnClickListener(new View.OnClickListener)(){
+//             @Override
+//             public void onClick(View v){
+//                 openviewpage();
+//             });
+//     }
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-
-import android.os.Bundle;
-
-import com.example.eskillsandroid.fragments.DiscussionsFragment;
-import com.example.eskillsandroid.fragments.HomeFragment;
-import com.example.eskillsandroid.fragments.SearchFragment;
-
- public class MainActivity extends AppCompatActivity {
-
-
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
 
 
