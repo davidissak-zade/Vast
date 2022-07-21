d8a5010094f9f6b82c85ac32331a5109c8646022
katiazacharia <109578632+katiazacharia@users.noreply.github.com>
2022-07-21 09:01:36 +0300

    clickable button
    cv button linked to viewpage

--- app/src/main/java/com/example/eskillsandroid/MainActivity.java
+++ app/src/main/java/com/example/eskillsandroid/MainActivity.java
@@ -40,22 +40,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.eskillsandroid.fragments.DiscussionsFragment;
 import com.example.eskillsandroid.fragments.HomeFragment;
 import com.example.eskillsandroid.fragments.SearchFragment;
 
 public class MainActivity extends AppCompatActivity {
-
-
-
+    private Button button;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void onCreate(Bundle savedinstanceState) {
+        super.onCreate(savedinstanceState);
         setContentView(R.layout.activity_main);
 
-
+        button = findViewById(R.id.button1);
 
     }
+    public void Onclick(View view){
+        Intent intent = new Intent (this, viewpage.class);
+        startActivity(intent);
+    }
 }
+
+
+
+
