f59ce4c3144cce3e5204099b1a22552cbcfee7ad
katiazacharia <109578632+katiazacharia@users.noreply.github.com>
2022-07-21 12:24:21 +0300

    comitting a messy code

--- app/src/main/java/com/example/eskillsandroid/MainActivity.java
+++ app/src/main/java/com/example/eskillsandroid/MainActivity.java
@@ -45,19 +45,41 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+<<<<<<< Updated upstream
 import com.example.eskillsandroid.fragments.DiscussionsFragment;
 import com.example.eskillsandroid.fragments.HomeFragment;
 import com.example.eskillsandroid.fragments.SearchFragment;
 
 public class MainActivity extends AppCompatActivity {
+=======
+ public class MainActivity extends AppCompatActivity {
+>>>>>>> Stashed changes
     private Button button;
     @Override
     protected void onCreate(Bundle savedinstanceState) {
         super.onCreate(savedinstanceState);
         setContentView(R.layout.activity_main);
 
+<<<<<<< Updated upstream
         button = findViewById(R.id.button1);
 
+=======
+        button = (Button) findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                openviewpage();
+            }
+        });
+    }
+
+     private void openviewpage() {
+     }
+
+     public void  opennviewpage(){
+        Intent intent = new Intent(this, viewpage.class);
+        startActivity(intent);
+>>>>>>> Stashed changes
     }
     public void Onclick(View view){
         Intent intent = new Intent (this, viewpage.class);
