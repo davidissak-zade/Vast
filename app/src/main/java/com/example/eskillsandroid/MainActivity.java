a53a340f01c9ae977178b2d1200ca71bb2f22528
LeMoR3X <74240551+LeMoR3X@users.noreply.github.com>
2022-07-23 01:42:44 +0300

    create post

--- app/src/main/java/com/example/eskillsandroid/MainActivity.java
+++ app/src/main/java/com/example/eskillsandroid/MainActivity.java
@@ -21,7 +21,7 @@ public  class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view)
             {
-                startActivity(new Intent(MainActivity.this, MemberCommunityActivity.class));
+                startActivity(new Intent(MainActivity.this, createpost.class));
             }
         });
 
