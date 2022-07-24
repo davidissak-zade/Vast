6cde9b3e9b7416815b0537774587104a7b18aa6c
David Issak-Zade <david.issak-zade@mail.ru>
2022-07-24 11:17:56 +0300

    .add backbutton

--- app/src/main/java/com/example/eskillsandroid/cvView.java
+++ app/src/main/java/com/example/eskillsandroid/cvView.java
@@ -2,7 +2,9 @@ package com.example.eskillsandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
@@ -12,6 +14,11 @@ public class cvView extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cv_view);
-
+        ImageButton BackButton = (ImageButton) findViewById(R.id.arrowbutton);
+        BackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(cvView.this, MainActivity.class));
+            }});
     }
 }
