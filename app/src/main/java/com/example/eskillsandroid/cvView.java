4e84df786bfb37271390d38b1a1ee56a631a8a25
David Issak-Zade <david.issak-zade@mail.ru>
2022-07-24 09:10:34 +0300

    .horizscrollview

--- app/src/main/java/com/example/eskillsandroid/cvView.java
+++ app/src/main/java/com/example/eskillsandroid/cvView.java
@@ -3,6 +3,8 @@ package com.example.eskillsandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class cvView extends AppCompatActivity {
 
@@ -10,5 +12,6 @@ public class cvView extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cv_view);
+
     }
 }
