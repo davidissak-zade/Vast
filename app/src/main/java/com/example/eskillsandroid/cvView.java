fb0b79d11683297d0422d87c9cf453d85c5643f9
LeMoR3X <74240551+LeMoR3X@users.noreply.github.com>
2022-07-25 15:21:23 +0300

    working on video player

--- app/src/main/java/com/example/eskillsandroid/cvView.java
+++ app/src/main/java/com/example/eskillsandroid/cvView.java
@@ -8,17 +8,29 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
+
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView;
+
 public class cvView extends AppCompatActivity {
+
+    private YouTubePlayerView youtubeplayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cv_view);
+
+        youtubeplayer = findViewById(R.id.activity_cvView_youtubePlayerView);
+        getLifecycle().addObserver(youtubeplayer);
+
         ImageButton BackButton = (ImageButton) findViewById(R.id.arrowbutton);
         BackButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(cvView.this, MainActivity.class));
             }});
+
+
     }
 }
