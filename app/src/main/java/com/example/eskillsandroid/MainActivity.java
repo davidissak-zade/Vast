58a225fecdd12e573a126275acbc70a568769682
David Issak-Zade <david.issak-zade@mail.ru>
2022-08-03 12:58:03 +0300

    ..

--- app/src/main/java/com/example/eskillsandroid/MainActivity.java
+++ app/src/main/java/com/example/eskillsandroid/MainActivity.java
@@ -18,16 +18,30 @@ public class MainActivity extends AppCompatActivity {
 
     ImageButton searchButton, HomeButton, DiscussionsButton, BankAccount, Renting, License, CV, scholarship
      ,visa;
-    TextView cvtxt,scholtxt,interviewtxt, examtxt,unitxt, banktxt,visatxt, savingtxt,loantxt, billtxt, taxestxt,licensetxt, renttxt, insuracetxt, cartxt,mailtxt,laundrytxt;
+    TextView cvtxt,scholtxt,interviewtxt, examtxt,unitxt, banktxt,visatxt, savingtxt,loantxt, billtxt, taxestxt,licensetxt, renttxt, insuracetxt, cartxt,mailtxt,laundrytxt, WelcomeTXT, Section1TXT, Section2TXT, Section3TXT;
+
+
     private String PageID;
     private String Language = "ENG";
     private Object AdapterView;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        int max = 9;
+        int min = 1;
+        int range = max - min + 1;
+        int rand = (int)(Math.random() * range) + min;
+
+        TextView message;
+        message = findViewById(R.id.message);
+
+        String[] quoteStr = {"We cannot solve problems with the kind of thinking we employed when we came up with them.” — Albert Einstein};", "Goal setting is the secret to a compelling future.” — Tony Robbins", "Learn as if you will live forever, live like you will die tomorrow", "Stay away from those people who try to disparage your ambitions. Small minds will always do that, but great minds will give you a feeling that you can become great too.", "When you change your thoughts, remember to also change your world.", "Success is not final; failure is not fatal: It is the courage to continue that counts.", "Success usually comes to those who are too busy looking for it.", "I never dreamed about success. I worked for it", "Success is getting what you want, happiness is wanting what you get."};
+        message.setText(quoteStr[rand-1]);
 
         Spinner mySpinner = findViewById(R.id.spinner1);
         String[] items = new String[]{"ENG", "ARB", "HEB"};
@@ -39,17 +53,85 @@ public class MainActivity extends AppCompatActivity {
             public void onItemSelected(android.widget.AdapterView<?> parent, View view, int i, long id) {
                 Log.d("INDENT", ""+i);
                 if (i == 0){
-                    mySpinner.setOnItemSelectedListener(this);
+                            mySpinner.setOnItemSelectedListener(this);
+                            WelcomeTXT.setText(R.string.introENG);
+                            Section1TXT.setText(R.string.Section1ENG);
+                            cvtxt.setText(R.string.CVENG);
+                            scholtxt.setText(R.string.ScholarshipsENG);
+                            interviewtxt.setText(R.string.InterviewENG);
+                            examtxt.setText(R.string.PsychometryENG);
+                            unitxt.setText(R.string.UniversityENG);
 
-                }else
-                    if(i == 1) {
-                        Language = "ARB";
+                            Section2TXT.setText(R.string.Section2ENG);
+                            banktxt.setText(R.string.BankENG);
+                            visatxt.setText(R.string.VisaENG);
+                            savingtxt.setText(R.string.SavingsENG);
+                            loantxt.setText(R.string.LoansENG);
+                            billtxt.setText(R.string.BillsENG);
+                            taxestxt.setText(R.string.TaxesENG);
 
-                }else
-                   if(i ==2){
-                       Language = "HEB";
+                            Section3TXT.setText(R.string.Section3ENG);
+                            licensetxt.setText(R.string.LicenseENG);
+                            renttxt.setText(R.string.RentENG);
+                            insuracetxt.setText(R.string.InsuranceENG);
+                            cartxt.setText(R.string.CarENG);
+                            mailtxt.setText(R.string.MailingENG);
+                            laundrytxt.setText(R.string.LaundryENG);
 
-                   }
+                         }
+                else if(i == 1) {
+                                    Language = "ARB";
+                                    WelcomeTXT.setText(R.string.introARB);
+                                    Section1TXT.setText(R.string.Section1ARB);
+                                    cvtxt.setText(R.string.CVARB);
+                                    scholtxt.setText(R.string.ScholarshipsARB);
+                                    interviewtxt.setText(R.string.InterviewARB);
+                                    examtxt.setText(R.string.PsychometryARB);
+                                    unitxt.setText(R.string.UniversityARB);
+
+                                    Section2TXT.setText(R.string.Section2ARB);
+                                    banktxt.setText(R.string.BankARB);
+                                    visatxt.setText(R.string.VisaARB);
+                                    savingtxt.setText(R.string.SavingsARB);
+                                    loantxt.setText(R.string.LoansARB);
+                                    billtxt.setText(R.string.BillsARB);
+                                    taxestxt.setText(R.string.TaxesARB);
+
+                                    Section3TXT.setText(R.string.Section3ARB);
+                                    licensetxt.setText(R.string.LicenseARB);
+                                    renttxt.setText(R.string.RentARB);
+                                    insuracetxt.setText(R.string.InsuranceARB);
+                                    cartxt.setText(R.string.CarARB);
+                                    mailtxt.setText(R.string.MailingARB);
+                                    laundrytxt.setText(R.string.LaundryARB);
+                                }
+
+                else if(i ==2)  {
+                                    Language = "HEB";
+                                    WelcomeTXT.setText(R.string.introHEB);
+                                    Section1TXT.setText(R.string.Section1HEB);
+                                    cvtxt.setText(R.string.CVHEB);
+                                    scholtxt.setText(R.string.ScholarshipsHEB);
+                                    interviewtxt.setText(R.string.InterviewHEB);
+                                    examtxt.setText(R.string.PsychometryHEB);
+                                    unitxt.setText(R.string.UniversityHEB);
+
+                                    Section2TXT.setText(R.string.Section2HEB);
+                                    banktxt.setText(R.string.BankHEB);
+                                    visatxt.setText(R.string.VisaHEB);
+                                    savingtxt.setText(R.string.SavingsHEB);
+                                    loantxt.setText(R.string.LoansHEB);
+                                    billtxt.setText(R.string.BillsHEB);
+                                    taxestxt.setText(R.string.TaxesHEB);
+
+                                    Section3TXT.setText(R.string.Section3HEB);
+                                    licensetxt.setText(R.string.LicenseHEB);
+                                    renttxt.setText(R.string.RentHEB);
+                                    insuracetxt.setText(R.string.InsuranceHEB);
+                                    cartxt.setText(R.string.CarHEB);
+                                    mailtxt.setText(R.string.MailingHEB);
+                                    laundrytxt.setText(R.string.LaundryHEB);
+                                }
             }
 
             @Override
@@ -58,9 +140,11 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
-
-
+        // binding the text holders
+        WelcomeTXT = findViewById(R.id.welcome);
+        Section1TXT = findViewById(R.id.section1);
+        Section2TXT = findViewById(R.id.section2);
+        Section3TXT = findViewById(R.id.section3);
         cvtxt = findViewById(R.id.cvtxt);
         scholtxt = findViewById(R.id.scholtxt);
         interviewtxt = findViewById(R.id.interviewtxt);
